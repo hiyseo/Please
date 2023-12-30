97a150fcc0030fa6217f02040dc33ab0e9ac1c42
Sumin-Y <sumin24@kaist.ac.kr>
2023-12-30 18:57:48 +0900

    20231230

--- app/src/main/java/com/example/week1/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/week1/ui/dashboard/DashboardFragment.kt
@@ -7,6 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.week1.R
 import com.example.week1.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -17,23 +22,79 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+//    private var recyclerView: RecyclerView? = null
+//    private var recyclerViewPhotoAdapter : RecyclerViewPhotoAdapter? = null
+//    private var photoList = mutableListOf<Photo>()
+//
+//    private fun preparePhotoListData() {
+//        var photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//        photo = Photo("dinner", R.drawable.todaydinner)
+//        photoList.add(photo)
+//
+//        recyclerViewPhotoAdapter!!.notifyDataSetChanged()
+//
+//    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+//        photoList = ArrayList()
+//
+//        recyclerView = findViewById<View>(R.id.ivPhotoImage) as RecyclerView
+//        recyclerViewPhotoAdapter = recyclerViewPhotoAdapter(this @DashboardFragment, photoList)
+//        val layoutManager : RecyclerView.LayoutManager = GridLayoutManager(this, 2)
+//        recyclerView!!.layoutManager = layoutManager
+//        recyclerView!!.adapter = recyclerViewPhotoAdapter
+//
+//        preparePhotoListData()
+
+//        val dashboardViewModel =
+//            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val photoList = ArrayList<Photo>()
+//        val recyclerViewPhotoAdapter = Photo(photoList)
+//
+//        binding.
+
+        val photoList = mutableListOf<Photo>()
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+        photoList.add(Photo("dinner", R.drawable.todaydinner))
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        val adapter = RecyclerViewPhotoAdapter(photoList)
+        recyclerView.adapter = adapter
+
         return root
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
---
+++ app/src/main/java/com/example/week1/ui/dashboard/Photo.kt
@@ -0,0 +1,4 @@
+package com.example.week1.ui.dashboard
+
+data class Photo(var name: String, var image: Int)
+
