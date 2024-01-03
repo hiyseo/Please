55322704d521bdae73802d5e4bfd5d2b12a14fe1
hiyseo <yoonseo@gim-yunseoui-MacBookPro-3.local>
2024-01-03 15:16:13 +0900

    1/3 15:16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("com.google.code.gson:gson:2.8.8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
